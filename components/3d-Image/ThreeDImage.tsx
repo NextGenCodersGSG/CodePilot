c490c9e04a7a4afcf259f20070ce920058c4ed27
Hadi87s <hadiirshaid8722@gmail.com>
2025-03-28 22:29:32 +0200

    Making the main image more rounded and added a border colors

--- components/3d-Image/ThreeDImage.tsx
+++ components/3d-Image/ThreeDImage.tsx
@@ -6,13 +6,13 @@ import { CardBody, CardContainer } from "@/components/ui/3d-card"
 export default function ThreeDImage() {
   return (
     <CardContainer className="inter-var">
-      <CardBody className="transition duration-200 bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.2] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto h-auto rounded-xl  border ">
+      <CardBody className="transition duration-300 bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.2] dark:bg-black w-auto h-auto rounded-3xl  border-2 dark:border-blue-950 dark:hover:border-emerald-900 ">
           <Image
             src="/Ai.jpg"
             width={1000}
             height={1000}
             alt="CodePilot dashboard preview"
-            className="rounded-lg h-auto w-full object-cover "
+            className="rounded-3xl h-auto w-full object-cover "
             priority
           />
       </CardBody>
