c8c6a05d008a7e0ae6f78f8573cb3e44768cf32f
Hadi87s <hadiirshaid8722@gmail.com>
2025-03-31 22:39:25 +0200

    feat: enhance error reporting and email template styling.

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import RightContent from '@/components/landing/right-content'
 import SplineLoader from '@/components/landing/spline-loader'
 
 const Page = () => {
+  throw new Error('This is a client component');
   return (
     <div>
       <LandingPage />
