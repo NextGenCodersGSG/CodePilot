03e0e91b4384e5600ab8886309a23894efc3abb3
LaraSamara <larasamara2002@gmail.com>
2025-04-04 20:03:32 +0300

    CP-42: Add Developer Backend

--- module/repositories/auth.repo.ts
+++ module/repositories/auth.repo.ts
@@ -1,9 +1,15 @@
 import userModel, { IUserDocument } from "@/DB/models/user.model";
+import { IUser } from "@/@types/index";
 
 export class UserRepository {
 
   async findUserByEmail(email: string): Promise<IUserDocument | null> {
     return await userModel.findOne({ email });
+  }
+
+  async AddDeveloper(data: IUser){
+    const developer = await userModel.create({...data});
+    return developer;
   }
 
 }
