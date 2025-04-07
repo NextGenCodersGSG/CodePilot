31fd518bcf80beb89ea6a226a8fe573bbc2bef2a
LaraSamara <larasamara2002@gmail.com>
2025-04-07 11:57:50 +0300

    CP-48: Add Get All Developers Backend

---
+++ app/api/all-developers/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+import { connection } from '@/DB/connection';
+import userService from '@/module/services/user.service';
+
+export async function GET() {
+    try {
+        await connection();
+        const developers = await userService.getAllDevelopers();
+        if(!developers.length){
+        return NextResponse.json({ error: 'No Developers Found' }, { status: 404 });
+
+        }
+        return NextResponse.json(developers);
+    } catch (error) {
+        console.error("Error fetching Developers:", error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+}
