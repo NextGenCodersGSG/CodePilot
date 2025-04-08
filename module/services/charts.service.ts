4a8e9f25ed639cb567758f436de0c8f5e6c73b7a
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2025-04-08 14:05:50 +0300

    Create the functionality of getting users roles data from database, create an API route to call it in frontend.

--- module/services/charts.service.ts
+++ module/services/charts.service.ts
@@ -1,7 +1,8 @@
-import { IChartData, ICodeReview, IUserFromDB } from "@/@types";
+import { IChartData, ICodeReview, IUser } from "@/@types";
 import CodeReviewRepo  from "@/module/repositories/reviews.repo";
 import getDayFromISO from "@/lib/getDayFromISO";
 import { convertToChartData } from "@/lib/convertToChartData";
+import  UserRepository  from "../repositories/user.repo";
 class ChartsService {
     async getReviewsData() {
         const reviews: ICodeReview[] = await CodeReviewRepo.getAllReviews();
@@ -14,6 +15,14 @@ class ChartsService {
         const chartData: IChartData[] =  convertToChartData(reviewsPerDay, "reviews");
         return chartData;
     }
-
+    async getUsersRolesData() {
+        const users: IUser[] = await UserRepository.findAllUsers();
+        const usersRoles: { [key: string]: number }= {};
+        users.map((user) => {
+            usersRoles[user.role] = (usersRoles[user.role] || 0) + 1;
+        });
+        const chartData: IChartData[] = convertToChartData(usersRoles, "value");
+        return chartData;
+    }
 }
 export default new ChartsService();
